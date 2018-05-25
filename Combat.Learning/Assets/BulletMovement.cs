2e58b0341cd2feed4d6e494949c2df5b2c03f516
vinsalegrado <vinsalegrado@gmail.com>
2018-05-26 02:39:48 +0800

    blackMamba bulletShoot

---
+++ Combat.Learning/Assets/BulletMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+	public int speed = 250;
+	public float timeToDestroy = 3f;
+
+	private void OnEnable() {
+		StartCoroutine(AutoDestroy(timeToDestroy));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * Time.deltaTime * speed);
+	}
+
+	IEnumerator AutoDestroy(float _time) {
+		yield return new WaitForSeconds(_time);
+		Destroy(gameObject);
+	}
+}
--- Combat.Learning/Assets/PlayerShoot.cs
+++ Combat.Learning/Assets/PlayerShoot.cs
@@ -8,6 +8,8 @@ public class PlayerShoot : MonoBehaviour {
 	GameObject bulletPrefab;
 	[SerializeField]
 	Transform weaponTip;
+	[SerializeField]
+	SpriteRenderer muzzleFlashRend;
 
 	public float fireRate = 5f;
 	public float damage = 25f;
@@ -19,6 +21,7 @@ public class PlayerShoot : MonoBehaviour {
 
 	public void Awake() {
 		c_movement = GetComponent<PlayerMovement>();
+		muzzleFlashRend.color = new Color(0,0,0,0);
 	}
 
 	public void OnShoot() {
@@ -36,7 +39,21 @@ public class PlayerShoot : MonoBehaviour {
 	void Shoot() {
 		Vector2 firePos = new Vector2(weaponTip.position.x, weaponTip.position.y);
 		Vector2 dir = (c_movement.m_FacingRight) ? Vector2.right : Vector2.left;
-		RaycastHit2D hit = Physics2D.Raycast(firePos, dir, range, whatToHit);
-		Debug.DrawRay(firePos, dir * range, Color.blue, 1f);
+		//RaycastHit2D hit = Physics2D.Raycast(firePos, dir, range, whatToHit);
+		//Debug.DrawRay(firePos, dir * range, Color.blue, 1f);
+		DrawBullet();
+	}
+
+	void DrawBullet() {
+		Quaternion rot = (c_movement.m_FacingRight) ? Quaternion.Euler(0,0,0) : Quaternion.Euler(0, 180, 0);
+		Instantiate(bulletPrefab, weaponTip.position, rot);
+		StartCoroutine(MuzzleFlash(0.02f));
+	}
+
+	IEnumerator MuzzleFlash(float _duration) {
+		muzzleFlashRend.color = new Color(1,1,1,1);
+		muzzleFlashRend.gameObject.transform.Rotate(Random.Range(0,2)*100,0,0);
+		yield return new WaitForSeconds(_duration);
+		muzzleFlashRend.color = new Color(0,0,0,0);
 	}
 }
